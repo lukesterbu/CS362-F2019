8a96c8a12bb09374e8b2974f8a75ebc87509af55
Luke <burrisl@oregonstate.edu>
2019-11-10 19:27:39 -0500

    Update unittest1.c

--- projects/burrisl/dominion/unittest1.c
+++ projects/burrisl/dominion/unittest1.c
@@ -1,49 +1,74 @@
 #include "dominion.h"
-#include "rngs.h"
+#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <assert.h>
+#include "rngs.h"
 
-int main(int argc, char** argv) {
-	// Unit test for Minion
-	// set your card array
-	int k[10] = { adventurer, council_room, feast, gardens, mine
-	, remodel, smithy, village, baron, great_hall };
-	// declare the game state
-	struct gameState G;
-	// declare the arrays
-	int coppers[MAX_HAND];
-	int silvers[MAX_HAND];
-	int golds[MAX_HAND];
-	printf("Begin Testing someFunction():\n");
-	
-	// set the state of your variables
-	// comment on what this is going to test
-	
-	memset(&G, 23, sizeof(struct gameState)); // set the game state
-	r = initializeGame(2, k, seed, &G); // initialize a new game
-	G.handCount[p] = handCount; // set any other variables
-	memcpy(G.hand[p], coppers, sizeof(int) * handCount);
-	// call the refactored function
-	myrefactoredfunction(p, &G, bonus);
-	// verify that your asserts pass, for the bugs your asserts may fail
-	assert(G.coins == something); // check a condition
-	assert(some condition);
-	assert(some condition);
+// Returns 1 if true and 0 if false
+int checkTrue(int x, int y, char* desc) {
+	if (int x == int y) {
+		printf("PASSED - %s", desc);
+	}
+	else {
+		printf("FAILED - %s", desc);
+	}
+}
 
-	// adjust the state of your variables to continue testing the function
-	// comment on what this is going to test
-	
-	memset(&G, 0, sizeof(struct gameState)); // set the game state
-	r = initializeGame(3, k, seed, &G); // initialize a new game
-	G.handCount[p] = handCount; // set any other variables
-	memcpy(G.hand[p], silvers, sizeof(int) * handCount);
-	// call the refactored function again
-	myrefactoredfunction(p, &G, bonus);
-	// verify that your asserts pass, for the bugs your asserts may fail
-	assert(G.coins == something); // check a condition
-	assert(some condition);
-	assert(some condition);
-	printf("Test completed!\n");
-	
-	return 0;
+int main () {
+    int r;
+    int choice1;
+    // This is a real value
+    int numPlayers = 2;
+    int currentPlayer = 0;
+    
+    int k[10] = {adventurer, council_room, feast, gardens, mine,
+        remodel, smithy, village, baron, great_hall};
+
+    struct gameState state;
+    
+    /***************************************************************************************
+    ** Test to see if an estate card is found in the hand when choice1 <= 0
+    ***************************************************************************************/
+    // Standard setup
+    memset(&state, 23, sizeof(struct gameState));
+    r = initializeGame(2, k, 618, &state);
+    state.handCount[currentPlayer] = 5;
+    for (int i = 0; i < state.handCount[currentPlayer]; i++) {
+    	state.hand[currentPlayer][i] = copper;
+    }
+    // Switch variables so test should pass
+    choice1 = 0;
+    state.hand[currentPlayer][4] = estate; // Set one of the cards to be an estate
+    state.coins = 0;
+    state.discardCount[currentPlayer] = 0;
+
+    // Call the function being tested;
+    doBaron(currentPlayer, choice1, &state);
+
+    // Do tests
+    checkTrue(state.coins, 4, "Coins Increased By 4");
+    checkTrue(state.discardCount[currentPlayer], 1, "Discard Count Increased By 1");
+    checkTrue(state.handCount[currentPlayer], 4, "Hand Count Decreased By 1");
+
+    /***************************************************************************************
+    ** Test to see if an estate card is not found in the hand when choice 1 <= 0
+    ***************************************************************************************/
+    // Standard setup
+    memset(&state, 23, sizeof(struct gameState));
+    r = initializeGame(2, k, 618, &state);
+    state.handCount[currentPlayer] = 5;
+    for (int i = 0; i < state.handCount[currentPlayer]; i++) {
+    	state.hand[currentPlayer][i] = copper;
+    }
+
+    // Switch variables so test should pass
+    choice1 = 0;
+
+    // Call the function being tested;
+    doBaron(currentPlayer, choice1, &state);
+
+    // Do tests
+    checkTrue(state.supplyCount[estate], 7, "Estate Supply decreased by 1"); // Should be one less than 8 since 2 players
+
+    return 0;
 }
