9cd6c24f4b4cd5ef295f57f9d80ef824dec7d58f
Luke <burrisl@oregonstate.edu>
2019-11-10 21:37:21 -0500

    Update unittest3.c

--- projects/burrisl/dominion/unittest3.c
+++ projects/burrisl/dominion/unittest3.c
@@ -88,5 +88,28 @@ int main(int argc, char** argv) {
     printFormatted("SUBTEST 2 - choice2 = -1.");
     checkTrue(doAmbassador(currentPlayer, choice1, choice2, &state, 0), -1, "Should Return -1.");
 
+    /***************************************************************************************
+    ** choice1 == handPos.
+    ***************************************************************************************/
+    // Standard Estate setup
+    memset(&state, 23, sizeof(struct gameState));
+    r = initializeGame(numPlayers, k, 618, &state);
+    state.handCount[currentPlayer] = 5;
+    for (int i = 0; i < state.handCount[currentPlayer]; i++) {
+    	state.hand[currentPlayer][i] = estate; // Set all of the cards to estates
+    }
+
+    // Switch variables so test should pass
+    choice1 = 0;
+    choice2 = 0;
+    state.coins = 0;
+    state.discardCount[currentPlayer] = 0;
+
+    // Call the function being tested;
+    doAmbassador(currentPlayer, choice1, choice2, &state, 0);
+
+    // Do tests
+    printFormatted("SUBTEST 3 - choice1 == handPos.");
+    checkTrue(doAmbassador(currentPlayer, choice1, choice2, &state, 0), -1, "Should Return -1.");
 	return 0;
 }
