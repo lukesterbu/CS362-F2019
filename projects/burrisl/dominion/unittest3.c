23cec4083509fc5e93d359c0191921269062fc99
Luke <burrisl@oregonstate.edu>
2019-11-10 21:48:24 -0500

    Update unittest3.c

--- projects/burrisl/dominion/unittest3.c
+++ projects/burrisl/dominion/unittest3.c
@@ -31,6 +31,7 @@ int main(int argc, char** argv) {
     // This is a real value
     int numPlayers = 2;
     int currentPlayer = 0;
+    int otherPlayer = 1;
     
     int k[10] = {adventurer, council_room, feast, gardens, mine,
         remodel, smithy, village, baron, great_hall};
@@ -111,5 +112,33 @@ int main(int argc, char** argv) {
     // Do tests
     printFormatted("SUBTEST 3 - choice1 == handPos.");
     checkTrue(doAmbassador(currentPlayer, choice1, choice2, &state, 0), -1, "Should Return -1.");
+	
+    /***************************************************************************************
+    ** Rest of Code
+    ***************************************************************************************/
+    // Standard Estate setup
+    memset(&state, 23, sizeof(struct gameState));
+    r = initializeGame(numPlayers, k, 618, &state);
+    state.handCount[currentPlayer] = 5;
+    for (int i = 0; i < state.handCount[currentPlayer]; i++) {
+    	state.hand[currentPlayer][i] = estate; // Set all of the cards to estates
+    }
+
+    // Switch variables so test should pass
+    choice1 = 1;
+    choice2 = 1;
+    state.coins = 0;
+    state.discardCount[currentPlayer] = 0;
+
+    // Store this for later since it will get changed once doAmbassador is called
+    state.supplyCount[state.hand[currentPlayer][choice1]] = 10;
+
+    // Call the function being tested;
+    doAmbassador(currentPlayer, choice1, choice2, &state, 0);
+
+    // Do tests
+    printFormatted("SUBTEST 4 - Rest of Code.");
+    checkTrue(state.supplyCount[tempCard], 11, "Supply Count Should Increase By 1.");
+    checkTrue(state.discardCount[otherPlayer], 1, "Discard Count Should Increase By 1.");
 	return 0;
 }
