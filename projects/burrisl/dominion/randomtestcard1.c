5bc706a6f6cac649cf78988bb420e955f3a50eb6
Luke <burrisl@oregonstate.edu>
2019-11-17 11:44:47 -0500

    Updated Random Test 1

--- projects/burrisl/dominion/randomtestcard1.c
+++ projects/burrisl/dominion/randomtestcard1.c
@@ -10,6 +10,11 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Makes certain function calls easier to understand
+#define TO_DISCARD 0
+#define TO_DECK 1
+#define TO_HAND 2
+
 // Prints out the description formatted with asterisks around it
 void printFormatted(char* desc) {
 	printf("**************************************************************\n");
@@ -28,46 +33,80 @@ void checkTrue(int x, int y, char* desc) {
 }
 
 int main() {
-    int r;
-    int choice1;
-    // This is a real value
-    int numPlayers = 2;
-    int currentPlayer = 0;
-    
-    int k[10] = {adventurer, council_room, feast, gardens, mine,
-        remodel, smithy, village, baron, great_hall};
-
     struct gameState state;
+    int numPlayers = 2; // May need to randomize this
+    int currentPlayer = 0; // May need to randomize this
+    // Estate Variables
+    int numOldEstatesInHand = 0;
+    int estateSupply = 0;
+    int numNewEstatesInHand = 0;
+
+    int numOrigTotalCards = 0;
+    int numCoinsBefore = 0;
+    int numCoinsAfter = 0;
+    int success = 0;
+    // Choice variables
+    int choice1 = 0;
+    int choice2 = 0;
+    int choice3 = 0;
+    // Supply piles
+    int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
+
     // Seed the random number generator
     srand(time(NULL));
-
-    // NEED TO EDIT FROM BELOW HERE
-
-    printFormatted("UNITTEST1 - doBaron()");
-    /***************************************************************************************
-    ** Test to see if an estate card is found in the hand when choice1 > 0
-    ***************************************************************************************/
-    // Standard Estate setup
+    // Initialize the Game
     memset(&state, 23, sizeof(struct gameState));
-    r = initializeGame(numPlayers, k, 618, &state);
-    state.handCount[currentPlayer] = 5;
-    for (int i = 0; i < state.handCount[currentPlayer]; i++) {
-    	state.hand[currentPlayer][i] = estate; // Set all of the cards to estates
+    initializeGame(numPlayers, k, 618, &state);
+    // Print out title of test
+    printFormatted("RANDOM TEST 1 - doBaron()");
+
+    // While loop variables
+    int currIter = 0;
+    int iterations = 10;
+
+    while (currIter < iterations) {
+        // Randomly set the size of the hand
+        state.handCount[currentPlayer] = rand() % 10;
+
+        // Add random cards to the hand
+        for (int card = 0; card < state.handCount[currentPlayer]; card++) {
+            state.hand[currentPlayer][card] = rand() % (treasure_map + 1);
+        }
+
+        // Gain a baron card
+        gainCard(baron, &state, TO_HAND, currentPlayer);
+
+        // Check how many estate cards are in the player's hand
+        for (int card = 0; card < numHandCards(&state); card++) {
+            if (handCard(card, &state) == estate) {
+                numOldEstatesInHand++;
+            }
+        }
+
+        numOrigTotalCards = fullDeckCount(currentPlayer, &state);
+        numCoinsBefore = updateCoins(currentPlayer, &state, 0); // Last parameter is bonus
+        estateSupply = supplyCount(estate, &state);
+
+        choice1 = rand() % 2; // Get a random choice1
+        // Play the baron card
+        success = playCard(numHandCards(&state) - 1, choice1, choice2, choice3, &state);
+        numCoinsAfter = updateCoins(currentPlayer, &state, 0);
+
+        checkTrue(success, 0, "Check if Card was Played Successfully");
+        checkTrue(state.numBuys, 2, "Check if numBuys == 2");
+
+        // Check how many estate cards are in the player's hand
+        for (int card = 0; card < numHandCards(&state); card++) {
+            if (handCard(card, &state) == estate) {
+                numNewEstatesInHand++;
+            }
+        }
+
+        endTurn(&state);
+        currIter++; // Increment Iterator
     }
-    // Switch variables so test should pass
-    choice1 = 1;
-    state.coins = 0;
-    state.discardCount[currentPlayer] = 0;
 
-    // Call the function being tested;
-    doBaron(currentPlayer, choice1, &state);
-
-    // Do tests
-    printFormatted("SUBTEST 1 - choice1 = 1. Check for Estate Card in Hand With All Estate Hand.");
-    checkTrue(state.numBuys, 2, "Buys Increased By 1."); // This will Fail because of my bug
-    checkTrue(state.coins, 4, "Coins Increased By 4.");
-    checkTrue(state.discardCount[currentPlayer], 1, "Discard Count Increased By 1.");
-    checkTrue(state.handCount[currentPlayer], 4, "Hand Count Decreased By 1.");
+    printFormatted("RANDOM TEST 1 COMPLETED - doBaron()");
 
     return 0;
 }
