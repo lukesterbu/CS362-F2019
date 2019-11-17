c0e65f978174d8dcf49279834be6055ec9fdc3ac
Luke <burrisl@oregonstate.edu>
2019-11-17 15:47:18 -0500

    Update randomtestcard2.c

--- projects/burrisl/dominion/randomtestcard2.c
+++ projects/burrisl/dominion/randomtestcard2.c
@@ -36,6 +36,7 @@ int main() {
     struct gameState state;
     int numPlayers = 2; // May need to randomize this
     int currentPlayer = 0; // May need to randomize this
+    int otherPlayer = 1;
 
     // Supply piles
     int k[10] = {adventurer, council_room, feast, gardens, minion, remodel, smithy, village, baron, great_hall};
@@ -53,7 +54,8 @@ int main() {
     while (currIter < iterations) {
         // Variable declarations
         int numCoinsBefore = 0;
-        int handSizeBefore = 0;
+        int handSizeBeforeCurrent = 0;
+        int handSizeBeforeOther = 0;
         // Choice variables
         int choice1 = 0;
         int choice2 = 0;
@@ -65,21 +67,20 @@ int main() {
 
         // Randomly set the size of the hand
         state.handCount[currentPlayer] = rand() % 10;
+        state.handCount[otherPlayer] = rand() % 10;
 
         // Add random cards to the hand
         for (int card = 0; card < state.handCount[currentPlayer]; card++) {
             state.hand[currentPlayer][card] = rand() % (treasure_map + 1);
         }
 
-        printf("Hand Count Before: %d\n", state.handCount[currentPlayer]);
         // Gain a minion card
         gainCard(minion, &state, TO_HAND, currentPlayer);
 
-        printf("Hand Count After: %d\n", state.handCount[currentPlayer]);
-
         // Get variable snapshots
         numCoinsBefore = state.coins;
-        handSizeBefore = state.handCount[currentPlayer];
+        handSizeBeforeCurrent = state.handCount[currentPlayer];
+        handSizeBeforeOther = state.handCount[otherPlayer];
 
         choice1 = rand() % 2; // Get a random choice1
         choice2 = rand() % 2; // Get a random choice2
@@ -91,10 +92,17 @@ int main() {
 
         // Player chooses gain 2 coins
         if (choice1 > 0) {
-            checkTrue(handSizeBefore - 1, state.handCount[currentPlayer], "Hand Size Decreased By 1");
+            checkTrue(handSizeBeforeCurrent - 1, state.handCount[currentPlayer], "Hand Size Decreased By 1");
             checkTrue(numCoinsBefore + 2, state.coins, "Coins Increased By 2");
         }
-
+        // Player selects other choice
+        else {
+            checkTrue(4, state.handCount[currentPlayer], "Current Player's Hand Size is Now 4");
+            // Only need to run this test if other player had more than 5 cards in their hand
+            if (handSizeBeforeOther >= 5) {
+                checkTrue(4, state.handCount[otherPlayer], "Other Player's Hand Size is Now 4");
+            }
+        }
         currIter++; // Increment Iterator
     }
 
